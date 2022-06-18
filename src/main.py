144a73845d1310dc0db83b71a6ed6da7bb19e02a
RobbieGTech <robbieg1515@gmail.com>
2022-06-18 11:42:22 -0400

    created basic file structure and such.

---
+++ src/main.py
@@ -0,0 +1,54 @@
+"""
+@ Author: Robert Greenslade
+@ Author: Anthony Ruffing
+@ Author: Joshua Stanley
+@
+@ Title: Pygame Template 
+@
+@ Date: June 28, 2021
+"""
+
+# Imports Needed
+import pygame
+import random
+
+# Constant Variables
+WIDTH = 640
+HEIGHT = 360
+FPS = 30
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Initializing Pygame and Window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tower Defense Game")
+clock = pygame.time.Clock()
+
+
+# Main Game Loop
+running = True
+while running:
+
+    # Process input (events)
+    for event in pygame.event.get():
+        
+        # Check for closing window
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Checking key presses
+        keys = pygame.key.get_pressed()
+    
+    # Update
+    pygame.display.update()
+    clock.tick(FPS)
+
+    # Draw / Render
+    screen.fill(BLACK)
